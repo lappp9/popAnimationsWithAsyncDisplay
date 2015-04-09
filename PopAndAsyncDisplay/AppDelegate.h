7e773d37996bb993b2196697b346e26677230311
Luke Parham <lappp9@gmail.com>
2015-04-09 18:18:56 +0900

    Initial Commit

---
+++ PopAndAsyncDisplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PopAndAsyncDisplay
+//
+//  Created by Luke Parham on 4/9/15.
+//  Copyright (c) 2015 Luke Parham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PopAndAsyncDisplay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PopAndAsyncDisplay
+//
+//  Created by Luke Parham on 4/9/15.
+//  Copyright (c) 2015 Luke Parham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
